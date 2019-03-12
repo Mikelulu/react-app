12f16e1dfd1f0f54c5b1e3d6451b55eea53b09e0
yangluke <yangluke@163.com>
2019-03-12 16:29:12 +0800

    Updates

---
+++ precache-manifest.cd060de1cb49e2f4a98f8535c9e14a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "51280510240d8590c8e8",
+    "url": "/react-app/static/js/main.496eabef.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-app/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "51280510240d8590c8e8",
+    "url": "/react-app/static/css/main.fce3ef48.chunk.css"
+  },
+  {
+    "revision": "b3d2bb461a6f78b9ee666e0f88dd1f9d",
+    "url": "/react-app/index.html"
+  }
+];
